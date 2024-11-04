024ec44f7b545923c68d5d8fbb678ae731ae5656
Alex Mikhankov <jesslich.nime@gmail.com>
2024-11-04 16:41:26 +0300

    55. Jump Game

---
+++ src/jump_game.rs
@@ -0,0 +1,35 @@
+// 55. Jump Game - https://leetcode.com/problems/jump-game/
+pub fn can_jump(nums: Vec<i32>) -> bool {
+    let mut jump_size = nums[0];
+
+    for i in 1..nums.len() {
+        if i <= (jump_size as usize) {
+            jump_size = std::cmp::max(jump_size, (i as i32) + nums[i])
+        }
+    }
+
+    jump_size >= ((nums.len() as i32) - 1)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_case_1() {
+        let nums = vec![2, 3, 1, 1, 4];
+        assert_eq!(can_jump(nums), true)
+    }
+
+    #[test]
+    fn test_case_2() {
+        let nums = vec![3, 2, 1, 0, 4];
+        assert_eq!(can_jump(nums), false)
+    }
+
+    #[test]
+    fn test_case_3() {
+        let nums = vec![1, 2, 3];
+        assert_eq!(can_jump(nums), true)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod max_consecutive_ones;
 mod maximum_subarray;
 mod majority_element;
+mod jump_game;
