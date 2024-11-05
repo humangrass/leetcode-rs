c4b4474710411fe377c3f1235a401e67896e69fa
Alex Mikhankov <jesslich.nime@gmail.com>
2024-11-05 21:59:38 +0300

    3090. Maximum Length Substring With Two Occurrences

--- src/maximum_length_substring_with_two_occurrences.rs
+++ src/maximum_length_substring_with_two_occurrences.rs
@@ -2,7 +2,7 @@ use std::cmp::max;
 use std::collections::HashMap;
 
 // 3090. Maximum Length Substring With Two Occurrences - https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/description/
-pub fn maximum_length_substring(s: String) -> i32 {
+pub fn maximum_length_substring_v1(s: String) -> i32 {
     let chars: Vec<char> = s.chars().collect();
 
     let max_repetitions = 2;
@@ -41,6 +41,33 @@ pub fn maximum_length_substring(s: String) -> i32 {
     result as i32
 }
 
+pub fn maximum_length_substring_v2(s: String) -> i32 {
+    let chars: Vec<char> = s.chars().collect();
+    let max_repetitions = 2;
+    let mut l = 0;
+    let mut result = 0;
+    let mut mapa = HashMap::new();
+
+    for r in 0..chars.len() {
+        *mapa.entry(chars[r]).or_insert(0) += 1;
+
+        while mapa[&chars[r]] > max_repetitions {
+            if let Some(count) = mapa.get_mut(&chars[l]) {
+                *count -= 1;
+                if *count == 0 {
+                    mapa.remove(&chars[l]);
+                }
+            }
+            l += 1;
+        }
+
+        // Update the result if the current subsegment satisfies max_repetitions the conditions
+        result = max(result, r - l + 1);
+    }
+
+    result as i32
+}
+
 
 #[cfg(test)]
 mod test {
@@ -49,12 +76,24 @@ mod test {
     #[test]
     fn test_case_1() {
         let s = "bcbbbcba".to_string();
-        assert_eq!(maximum_length_substring(s), 4)
+        assert_eq!(maximum_length_substring_v1(s), 4)
     }
 
     #[test]
     fn test_case_2() {
         let s = "aaaa".to_string();
-        assert_eq!(maximum_length_substring(s), 2)
+        assert_eq!(maximum_length_substring_v1(s), 2)
+    }
+
+    #[test]
+    fn test_case_3() {
+        let s = "bcbbbcba".to_string();
+        assert_eq!(maximum_length_substring_v2(s), 4)
+    }
+
+    #[test]
+    fn test_case_4() {
+        let s = "aaaa".to_string();
+        assert_eq!(maximum_length_substring_v2(s), 2)
     }
 }
