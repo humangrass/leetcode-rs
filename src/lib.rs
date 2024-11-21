3b3fffd6274a37bb0d036ab2207fe8da0180dfc2
Alex Mikhankov <jesslich.nime@gmail.com>
2024-11-22 00:40:56 +0300

    283. Move Zeroes

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ mod majority_element;
 mod jump_game;
 mod maximum_length_substring_with_two_occurrences;
 mod best_time_to_buy_and_sell_stock;
+mod move_zeroes_283;
---
+++ src/move_zeroes_283.rs
@@ -0,0 +1,32 @@
+// 283. Move Zeroes - https://leetcode.com/problems/move-zeroes/description/
+pub fn move_zeroes(nums: &mut Vec<i32>) {
+    let mut index = 0;
+    for i in 0..nums.len() {
+        if nums[i] != 0 {
+            nums[index] = nums[i];
+            index += 1;
+        }
+    }
+    for i in index..nums.len() {
+        nums[i] = 0;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_case_1() {
+        let nums = &mut vec![0,1,0,3,12];
+        move_zeroes(nums);
+        assert_eq!(nums, &mut vec![1,3,12,0,0])
+    }
+
+    #[test]
+    fn test_case_2() {
+        let nums = &mut vec![0];
+        move_zeroes(nums);
+        assert_eq!(nums, &mut vec![0])
+    }
+}
