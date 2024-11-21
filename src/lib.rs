0ac171e3937fe189827982da09fa736cc7eaaac4
Alex Mikhankov <jesslich.nime@gmail.com>
2024-11-22 00:43:15 +0300

    55. Jump Game

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 mod max_consecutive_ones_485;
 mod maximum_subarray_53;
 mod majority_element_169;
-mod jump_game;
+mod jump_game_55;
 mod maximum_length_substring_with_two_occurrences_3090;
 mod best_time_to_buy_and_sell_stock;
 mod move_zeroes_283;
