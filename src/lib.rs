cce729cd30c72540f7e122786351be0f7a1eaf29
Alex Mikhankov <jesslich.nime@gmail.com>
2024-11-22 00:42:01 +0300

    3090. Maximum Length Substring With Two Occurrences

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ mod max_consecutive_ones;
 mod maximum_subarray_53;
 mod majority_element;
 mod jump_game;
-mod maximum_length_substring_with_two_occurrences;
+mod maximum_length_substring_with_two_occurrences_3090;
 mod best_time_to_buy_and_sell_stock;
 mod move_zeroes_283;
