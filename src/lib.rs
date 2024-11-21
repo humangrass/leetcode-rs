7aaa833985728551de8c8307e6b20b3ad0a2dcd4
Alex Mikhankov <jesslich.nime@gmail.com>
2024-11-22 00:42:53 +0300

    169. Majority Element

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod max_consecutive_ones_485;
 mod maximum_subarray_53;
-mod majority_element;
+mod majority_element_169;
 mod jump_game;
 mod maximum_length_substring_with_two_occurrences_3090;
 mod best_time_to_buy_and_sell_stock;
